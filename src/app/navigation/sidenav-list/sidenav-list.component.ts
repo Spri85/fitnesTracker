8e343d44d5d8d89e0703387b450388ee96876a19
Viktor <viktorpar@gmail.com>
2018-03-23 08:34:27 +0200

    base

---
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { Subscription } from 'rxjs/Subscription';
+
+@Component({
+  selector: 'app-sidenav-list',
+  templateUrl: './sidenav-list.component.html',
+  styleUrls: ['./sidenav-list.component.css']
+})
+export class SidenavListComponent implements OnInit {
+  isAuth: boolean;
+  authSubscription: Subscription;
+  @Output() closeSidenav = new EventEmitter<void>();
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
+  }
+
+  onClose() {
+    this.closeSidenav.emit();
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.onClose();
+  }
+
+}
---
+++ src/app/training/past-training/past-training.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { Exercise } from '../exercise.model';
+import { TrainingService } from '../training.service';
+
+@Component({
+  selector: 'app-past-training',
+  templateUrl: './past-training.component.html',
+  styleUrls: ['./past-training.component.css']
+})
+export class PastTrainingComponent implements OnInit {
+  displayedColumns = ['date', 'name', 'calories', 'duration', 'state'];
+  dataSource = new MatTableDataSource<Exercise>();
+
+  constructor(private trainingService: TrainingService) { }
+
+  ngOnInit() {
+    this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
+  }
+
+}
